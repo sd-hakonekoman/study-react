b9280992f8d7c8ab8880e94350895286cc9ea1c4
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2023-06-08 09:45:40 +0900

    fix: ch3

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Footer } from '@/components/Footer'
 import { Links } from '@/components/Links'
-import Headline from '@/components/Headline'
+import {Headline} from '@/components/Headline'
 
 const inter = Inter({ subsets: ['latin'] })
 
