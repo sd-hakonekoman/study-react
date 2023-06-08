01cac2c1a54fa2901d768b7e0f4e8b3cab226b5e
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2023-06-08 09:17:04 +0900

    ch3

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Links } from '@/components/Links'
+import Headline from '@/components/Headline'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +19,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+          <Headline title="Index page" />
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
