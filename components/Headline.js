f85d393790e69b7603a1c790faa5da5191613be1
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2023-06-08 09:19:32 +0900

    ch3 - 追加

---
+++ components/Headline.js
@@ -0,0 +1,19 @@
+// import React from "react"
+import styles from '@/styles/Home.module.css'
+
+export default function Headline(props) {
+  console.log(props)
+
+  const page = props.page || `index`
+
+  return (
+    <div>
+      <h1>見出し：{props.title}</h1>
+
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>pages/{page}.js</code>
+      </p>
+    </div>
+  )
+}
