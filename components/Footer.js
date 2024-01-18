fbb3ba30e32374edc822700ea6b9c86d1520f4b9
sd-abe <sd-hakonekoman@users.noreply.github.com>
2024-01-18 20:05:16 +0900

    全部削除

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import styles from '@/components/Footer.module.css'
-
-export function Footer() {
-  return (
-    <footer className={styles['c-footer']}>
-      <p>これはフッター</p>
-    </footer>
-  )
-}
