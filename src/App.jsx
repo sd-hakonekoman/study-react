4a237dbebcf76cbb5677bf7307563943d52ebcd0
sd-hakonekoman <sd-hakonekoman@users.noreply.github.com>
2024-12-22 23:18:23 +0900

    ch6

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback, memo } from "react"
 import { ColorMessage } from "./components/ColorMessage"
+import { Child1 } from "./components/ch6/Child1"
+import { Child4 } from "./components/ch6/Child4"
 
-export const App = () => {
+export const App = memo(() => {
   // レンダリングの確認用
-  console.log(`レンダリング`)
+  console.log(`App レンダリング`)
 
   // State の定義
   const [num, setNum] = useState(0)
@@ -12,26 +14,35 @@ export const App = () => {
     setNum((num) => num + 1)
   }
 
-  const contentStyle = {
-    color: "pink",
-    fontSize: "calc(20px - 2px)"
-  }
+  const onClickReset = useCallback(() =>{
+    setNum((num) => 0)
+  }, [])
 
-  // useEffect
-  useEffect(()=>{
-    console.log(`useEffect()を読んでます`)
-  }, [num])
+  // const contentStyle = {
+  //   color: "pink",
+  //   fontSize: "calc(20px - 2px)"
+  // }
+
+  // // useEffect
+  // useEffect(()=>{
+  //   console.log(` ()を読んでます`)
+  // }, [num])
 
   return(
     <>
-      <h1 style={{color: "#f0f"}}>こんにちは?2</h1>
+      {/* <h1 style={{color: "#f0f"}}>こんにちは?2</h1>
       <ColorMessage color="skyblue">
         <span style={{color: "orange"}}>あなたは</span>
         <span>お元気ですか？</span>
       </ColorMessage>
       <p style={contentStyle}>お元気で！</p>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p> */}
+
+      <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
+      <Child1 onClickReset={onClickReset} />
+      <Child4 />
     </>
   );
-}
+})
--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import { Emotion } from "./components/Emotion";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-const ch5 = ReactDOM.createRoot(document.getElementById('ch5'));
-ch5.render(<CssModules />)
+// const ch5 = ReactDOM.createRoot(document.getElementById('ch5'));
+// ch5.render(<CssModules />)
 
-const ch5StyledJsx = ReactDOM.createRoot(document.getElementById('ch5-styledJsx'));
-ch5StyledJsx.render(<StyledJsx />)
+// const ch5StyledJsx = ReactDOM.createRoot(document.getElementById('ch5-styledJsx'));
+// ch5StyledJsx.render(<StyledJsx />)
 
-const ch5StyledComponents = ReactDOM.createRoot(document.getElementById('ch5-styledComponents'));
-ch5StyledComponents.render(<StyledComponents />)
+// const ch5StyledComponents = ReactDOM.createRoot(document.getElementById('ch5-styledComponents'));
+// ch5StyledComponents.render(<StyledComponents />)
 
-const emotion = ReactDOM.createRoot(document.getElementById('emotion'));
-emotion.render(<Emotion />)
+// const emotion = ReactDOM.createRoot(document.getElementById('emotion'));
+// emotion.render(<Emotion />)
